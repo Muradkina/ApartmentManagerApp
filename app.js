f374eb8394dea42cb4f9411a163e49a2e9afefcc
Murat Kına <muradkina@ycp.com.tr>
2023-03-10 21:55:21 +0300

    laout entege

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 import express from "express";
+import path from "path";
+import expressLayouts from "express-ejs-layouts";
 
 const app = express();
 const port = 3000;
 
 
+// assets dosya yolu
+const assetsPath = path.join(path.resolve(), "assests");
+app.use(express.static(assetsPath));
+
+// ejs template engine
+app.use(expressLayouts);    
+app.set('view engine', 'ejs');
+
+// Ana sayfa get isteği
+app.get('/', (req, res) => {
+    res.render("index",{ layout: "./_Layout"})
+});
+
 app.listen(port, () => {
-    console.log(`Application running on port: ${port}`);
+    console.log(`Uygulama ${port} numaralı portta çalışıyor`);
 });
